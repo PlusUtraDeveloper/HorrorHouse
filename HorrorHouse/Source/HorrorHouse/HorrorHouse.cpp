b526ab4f64e23ce182ff3545410d674489b65a6b
MJ-ACER\Admin <meljohncatanaoan@gmail.com>
2016-11-19 17:05:40 -0800

    Created Game Folder

---
+++ HorrorHouse/Source/HorrorHouse/HorrorHouse.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HorrorHouse.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HorrorHouse, "HorrorHouse" );
---
+++ HorrorHouse/Source/HorrorHouse/HorrorHouseGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "HorrorHouseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORHOUSE_API AHorrorHouseGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
